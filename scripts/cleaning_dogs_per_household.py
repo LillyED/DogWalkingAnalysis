0cfdd7200e6934fac70b2fc8f36c448839662e96
Lilly <lilly.elouise@gmail.com>
2024-12-03 17:07:21 +0000

    Update cleaning_dogs_per_household.py
    Annotating my work.

--- scripts/cleaning_dogs_per_household.py
+++ scripts/cleaning_dogs_per_household.py
@@ -1,12 +1,16 @@
+#Install and load the data
+!pip install pandas
 import pandas as pd
-
 data = pd.read_csv('Dogs_Per_Household.csv')
 
+#Inspect the data
 print(data.info)
 print(data.head)
 
+#Cleaning the data
 data = data.dropna()
 
 data['PostcodeDistrict'] = data['PostcodeDistrict'].str.upper()
 
+#Saving the clean data
 data.to_csv('cleaned_dogs_per_household.csv', index=False)
