50be020370ddb2af5f43479e2cc5ca72234816c8
Lilly <lilly.elouise@gmail.com>
2024-12-03 18:34:33 +0000

    Update cleaning_dogs_per_household.py
    Mapping postcodes and deleting unneeded columns.

--- scripts/cleaning_dogs_per_household.py
+++ scripts/cleaning_dogs_per_household.py
@@ -13,7 +13,22 @@ data = data.dropna()
 data['PostcodeDistrict'] = data['PostcodeDistrict'].str.upper()
 
 #Changing column names to make coding easier
-data.rename(columns={'PostcodeDistrict': 'postcode', 'DogsPerHousehold': 'dogs'})
+data = data.rename(columns={'PostcodeDistrict': 'postcode', 'DogsPerHousehold': 'dogs'})
+
+# Mapping postcodes to towns (used ai to help with this part)
+postcode_to_town = {
+    'BR3': 'beckenham', 'BR1': 'bromley', 'BR2': 'bromley', 'SE9': 'mottingham',
+    'TN13': 'sevenoaks', 'TN14': 'sevenoaks', 'TN15': 'sevenoaks', 'BR5': 'orpington',
+    'BR6': 'orpington', 'BR7': 'chislehurst', 'SE12': 'lee', 'SE6': 'bellingham',
+    'DA14': 'sidcup', 'DA15': 'sidcup', 'DA1': 'dartford', 'DA2': 'dartford',
+    'TN1': 'tunbridge wells', 'TN2': 'tunbridge wells', 'TN3': 'tunbridge wells', 'TN4': 'tunbridge wells'
+}
+
+# Function to map postcodes to towns (used ai to help with this part)
+data['town'] = data['postcode'].apply(lambda x: next((town for code, town in postcode_to_town.items() if str(x).startswith(code)), 'unknown'))
+
+# Remove rows where 'town' is 'unknown'
+data = data[data['town'] != 'unknown']
 
 #Saving the clean data
 data.to_csv('cleaned_dogs_per_household.csv', index=False)
